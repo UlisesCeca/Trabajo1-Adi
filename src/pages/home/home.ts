7d6273ebc3fd3282d17f7f35505bcb17e8b7135c
UlisesCeca <ulises@ulisesceca.com>
2018-09-29 16:25:42 +0200

    terminado todos los pasos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,16 +6,31 @@ import { HttpProvider } from '../../providers/http/http';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  public messages: string[];
-  public loading: boolean;
+  public messages1: string[];
+  public messages2: string[];
+  public messages3: string[];
+  public messages4: string[];
+  public loading1: boolean;
+  public loading2: boolean;
+  public loading3: boolean;
+  public loading4: boolean;
   private step1Value: number;
   private step2Value: number;
   private step3Value: number;
-  private
+  private step4value: string;
+  private step5value: any;
+  private step6value: any;
 
   constructor(private http: HttpProvider) {
-    this.messages = [];
-    this.loading = true;
+    this.messages1 = [];
+    this.messages2 = [];
+    this.messages3 = [];
+    this.messages4 = [];
+    this.loading1 = true;
+    this.loading2 = true;
+    this.loading3 = true;
+    this.loading4 = true;
+    this.step4value = "05706851";
   }
 
   public ionViewDidEnter(): void {
@@ -23,59 +38,72 @@ export class HomePage {
   }
 
   private async start(): Promise<void> {
-    await this.step1();
-    await this.step2();
-    await this.step3();
-    await this.step4();
+    await this.step11();
+    await this.step12();
+    await this.step13();
+    await this.step14();
+    await this.step21();
+    await this.step22();
+    await this.step23();
+    await this.step24();
+    await this.step31();
+    await this.step32();
+    await this.step33();
+    await this.step34();
+    await this.step41();
+    await this.step42();
+    await this.step43();
+    await this.step44();
+    await this.step45();
   }
 
-  private step1(): Promise<void> {
+  private step11(): Promise<void> {
     return new Promise(resolve => {
-      this.http.step1().then(data => {
+      this.http.step11().then(data => {
         this.step1Value = data.value;
-        this.messages.push("Paso 1 finalizado. Valor obtenido: " + data.value);
+        this.messages1.push("Paso 1.1 finalizado. Valor obtenido: " + data.value);
         resolve();
       });
     });
   }
 
-  private step2(): Promise<void> {
+  private step12(): Promise<void> {
     return new Promise(resolve => {
-      this.http.step2().then(data => {
+      this.http.step12().then(data => {
         this.step2Value = data.value;
-        this.messages.push("Paso 2 finalizado. Valor obtenido: " + data.value);
+        this.messages1.push("Paso 1.2 finalizado. Valor obtenido: " + data.value);
         resolve();
       });
     });
   }
 
-  private step3(): Promise<void> {
+  private step13(): Promise<void> {
     return new Promise(resolve => {
-      this.http.step3().then(data => {
+      this.http.step13().then(data => {
         this.step3Value = data.value;
         switch (data.value) {
           case "-":
             this.step3Value = this.step1Value - this.step2Value;
-            this.messages.push("Paso 3 finalizado. La operación es una resta. Resultado: " + this.step1Value + " - " + this.step2Value + " = " + this.step3Value);
+            this.messages1.push("Paso 1.3 finalizado. La operación es una resta. Resultado: " + this.step1Value + " - " + this.step2Value + " = " + this.step3Value);
             break;
 
           case "+":
             this.step3Value = this.step1Value + this.step2Value;
-            this.messages.push("Paso 3 finalizado. La operación es una suma. Resultado: " + this.step1Value + " + " + this.step2Value + " = " + this.step3Value);
+            this.messages1.push("Paso 1.3 finalizado. La operación es una suma. Resultado: " + this.step1Value + " + " + this.step2Value + " = " + this.step3Value);
             break;
 
           case "/":
             this.step3Value = this.step1Value / this.step2Value;
-            this.messages.push("Paso 3 finalizado. La operación es una división. Resultado: " + this.step1Value + " / " + this.step2Value + " = " + this.step3Value);
+            this.messages1.push("Paso 1.3 finalizado. La operación es una división. Resultado: " + this.step1Value + " / " + this.step2Value + " = " + this.step3Value);
             break;
 
           case "*":
             this.step3Value = this.step1Value * this.step2Value;
-            this.messages.push("Paso 3 finalizado. La operación es una multiplicación. Resultado: " + this.step1Value + " * " + this.step2Value + " = " + this.step3Value);
+            this.messages1.push("Paso 1.3 finalizado. La operación es una multiplicación. Resultado: " + this.step1Value + " * " + this.step2Value + " = " + this.step3Value);
             break;
 
           default:
-            this.messages.push("Paso 3 finalizado. No se ha podido determinar el tipo de operacion");
+            this.messages1.push("Paso 1.3 finalizado. No se ha podido determinar el tipo de operacion");
             break;
         }
         resolve();
@@ -83,11 +111,138 @@ export class HomePage {
     });
   }
 
-  private step4(): Promise<void> {
+  private step14(): Promise<void> {
     return new Promise(resolve => {
-      this.http.step4(this.step3Value).then(data => {
-        this.messages.push("Paso 4 finalizado. Respuesta del servidor: " + JSON.stringify(data));
-        this.loading = false;
+      this.http.step14(this.step3Value).then(data => {
+        this.messages1.push("Paso 1.4 finalizado. Respuesta del servidor: " + JSON.stringify(data));
+        this.loading1 = false;
+        resolve();
+      });
+    });
+  }
+
+  private step21(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step21().then(data => {
+        this.messages2.push("Paso 2.1 finalizado. Acción: " + data.action);
+        resolve();
+      });
+    });
+  }
+
+  private step22(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step22(this.step4value).then(data => {
+        this.messages2.push("Paso 2.2 finalizado. Respuesta del servidor: " + JSON.stringify(data));
+        resolve();
+      });
+    });
+  }
+
+  private step23(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step23().then(data => {
+        this.messages2.push("Paso 2.3 finalizado. Siguiente acción: " + data.action);
+        this.loading2 = false;
+        resolve();
+      });
+    });
+  }
+
+  private step24(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step24().then(data => {
+        if (this.step4value == data.value) this.messages2.push("Paso 2.4 finalizado. Valor: " + data.value + ", coincide");
+        else this.messages2.push("Paso 2.4 finalizado. Valor: " + data.value + ", no coincide");
+        this.loading2 = false;
+        resolve();
+      });
+    });
+  }
+  
+  private step31(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step31().then(data => {
+        this.messages3.push("Paso 3.1 finalizado. Vamos a borrar el primer valor de los siguientes: " + data.value);
+        this.step5value = data.value[0];
+        resolve();
+      });
+    });
+  }
+  
+  
+  private step32(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step32(this.step5value).then(data => {
+        this.messages3.push("Paso 3.2 finalizado. Borrado primer elemento. Se ha borrado: " + this.step5value);
+        resolve();
+      });
+    });
+  }
+  
+  
+  private step33(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step33().then(data => {
+        this.messages3.push("Paso 3.3 finalizado. Respuesta petición: " + data.action);
+        this.loading3 = false;
+        resolve();
+      });
+    });
+  }
+  
+  private step34(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step31().then(data => {
+        this.messages3.push("Paso 3.4 finalizado. Valores después del DEL: " + data.value);
+        this.loading3 = false;
+        resolve();
+      });
+    });
+  }
+  
+  private step41(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step41().then(data => {
+        this.messages4.push("Paso 4.1 finalizado. Respuesta petición: " + data.action);
+        resolve();
+      });
+    });
+  }
+  
+  private step42(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step42().then(data => {
+        this.messages4.push("Paso 4.2 finalizado. Métodos permitidos: " + data.headers.get("Allow"))
+        this.step6value = data.headers.get("Allow");
+        resolve();
+      });
+    });
+  }
+  
+  private step44(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step44().then(data => {
+        this.messages4.push("Paso 4.4 finalizado. Respuesta: " + JSON.stringify(data));
+        resolve();
+      });
+    });
+  }
+  
+  private step45(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step45().then(data => {
+        this.messages4.push("Paso 4.5 finalizado. Respuesta petición: " + JSON.stringify(data));
+        this.loading4 = false;
+        resolve();
+      });
+    });
+  }
+  
+  private step43(): Promise<void> {
+    return new Promise(resolve => {
+      this.http.step43(this.step6value).then(data => {
+        this.messages4.push("Paso 4.3 finalizado. Escritos metodos a /options: " + JSON.stringify(data));
         resolve();
       });
     });
--- src/providers/http/http.ts
+++ src/providers/http/http.ts
@@ -1,27 +1,80 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from '../../../node_modules/rxjs/Observable';
 
 @Injectable()
 export class HttpProvider {
 
   constructor(private http: HttpClient) { }
 
-  public step1(): Promise<any> {
+  public step11(): Promise<any> {
     return this.http.get("http://httptask-142719.appspot.com/05706852/step1").toPromise();
   }
   
-  public step2(): Promise<any> {
+  public step12(): Promise<any> {
     return this.http.get("http://httptask-142719.appspot.com/05706852/op2").toPromise();
   }
   
-  public step3(): Promise<any> {
+  public step13(): Promise<any> {
     return this.http.get("http://httptask-142719.appspot.com/05706852/operator").toPromise();
   }
   
-  public step4(value: number): Promise<any> {
+  public step14(value: number): Promise<any> {
     const headers: HttpHeaders = new HttpHeaders()
     .append("Content-Type", "application/json");
-    return this.http.post("http://httptask-142719.appspot.com/05706852/results", "{ \"value\": " + value + "}", {headers: headers}).toPromise();
+    return this.http.post("http://httptask-142719.appspot.com/05706852/results", { value: value }, {headers: headers}).toPromise();
+  }
+  
+  public step21(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/step2").toPromise();
+  }
+  
+  public step22(value: string): Promise<any> {
+    const headers: HttpHeaders = new HttpHeaders()
+    .append("Content-Type", "application/json");
+    return this.http.put("http://httptask-142719.appspot.com/05706852/dni", { value: value }, {headers: headers}).toPromise();
+  }
+
+  public step23(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/checkPUT").toPromise();
+  }
+  
+  public step24(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/dni").toPromise();
+  }
+  
+  public step31(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/step3").toPromise();
+  }
+  
+  public step32(value: any): Promise<any> {
+    return this.http.delete("http://httptask-142719.appspot.com/05706852/delete/" + value).toPromise();
+  }
+  
+  public step33(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/checkDEL").toPromise();
+  }
+
+  public step41(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/step4").toPromise();
+  }
+  
+  public step42(): Promise<any> {
+    return this.http.options("http://httptask-142719.appspot.com/05706852/options", {observe: 'response'}).toPromise()
+  }
+  
+  public step44(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/checkOPT").toPromise()
+  }
+  
+  public step45(): Promise<any> {
+    return this.http.get("http://httptask-142719.appspot.com/05706852/options").toPromise()
+  }
+  
+  public step43(value: any): Promise<any> {
+    const headers: HttpHeaders = new HttpHeaders()
+    .append("Content-Type", "application/json");
+    return this.http.put("http://httptask-142719.appspot.com/05706852/options", {value: value}, {headers: headers}).toPromise()
   }
 
 }
